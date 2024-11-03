9bf4ee3f5adf1803e25771336d13af723ad7fee7
leonel-giuliano <129548024+leonel-giuliano@users.noreply.github.com>
2024-11-03 15:39:47 -0300

    Added file and line desc.

--- src/errnum.c
+++ src/errnum.c
@@ -16,7 +16,8 @@ int errnum(err_t e, ...) {
     va_start(arg, e);
 
 
-    if(errnum_program_name != NULL) fprintf(stderr, "%s: ", errnum_program_name);
+    if(errnum_program_name != NULL) fprintf(stderr, "%s ", errnum_program_name);
+    fprintf(stderr, "(%s:%d): ", __FILE__, __LINE__);
 
     switch (e) {
         case ERR_ARG:
